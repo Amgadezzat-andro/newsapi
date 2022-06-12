38530b9f7331d817120aeed5531e60aee0b4a9c7
Amgadezzat-andro <megostar59@gmail.com>
2022-06-12 22:11:07 +0200

    post,categories,comment

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title', 'content', 'date_written',
+        'featured_image', 'vote_up', 'vote_down', 'user_id', 'category_id'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+}
---
+++ database/migrations/2022_06_12_180145_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->dateTime('date_written');
+            $table->text('content');
+            $table->string('featured_image')->nullable();
+            $table->integer('vote_up')->nullable();
+            $table->integer('vote_down')->nullable();
+
+            //Relationships
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
