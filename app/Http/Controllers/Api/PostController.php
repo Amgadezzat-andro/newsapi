54ef4544403dcc663e59b2012dfc89db0908eee4
Amgadezzat-andro <megostar59@gmail.com>
2022-06-13 16:02:13 +0200

    finish get data on api design

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostsResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::paginate(env('POST_PER_PAGE'));
+        return new PostsResource($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+
+        $post = Post::find($id);
+        return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function comments($id)
+    {
+        $post = Post::find($id);
+        $comments = $post->comments()->paginate(env('POST_PER_PAGE'));
+        return new CommentResource($comments);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,16 @@ Route::get('authors/{id}', 'App\Http\Controllers\Api\UserController@show');
 Route::get('posts/author/{id}', 'App\Http\Controllers\Api\UserController@posts');
 Route::get('comments/author/{id}', 'App\Http\Controllers\Api\UserController@comments');
 
+
+//Post Related
+Route::get('categories','App\Http\Controllers\Api\CategoryController@index');
+Route::get('posts/categories/{id}','App\Http\Controllers\Api\CategoryController@posts');
+Route::get('posts','App\Http\Controllers\Api\PostController@index');
+Route::get('posts/{id}','App\Http\Controllers\Api\PostController@show');
+Route::get('comments/posts/{id}','App\Http\Controllers\Api\PostController@comments');
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
