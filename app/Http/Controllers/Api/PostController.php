402474fbacc65e0044488856fad6a119cb4ab06d
Amgadezzat-andro <megostar59@gmail.com>
2022-06-14 22:31:34 +0200

    make destroy post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -191,7 +191,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        $post = Post::destroy($id);
+        $post = Post::find($id);
+        $post->delete();
         return new PostResource($post);
     }
 
